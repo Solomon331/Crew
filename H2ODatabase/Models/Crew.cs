118cbafd468725d9ceb6ddfacaf8c35fac1126c4
Solomon331 <angelolivera331@gmail.com>
2017-07-28 19:40:47 -0400

    Add project files.

---
+++ H2ODatabase/Models/Crew.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace H2ODatabase.Models
+{
+    public class Crew
+    {
+        public int CrewID { get; set; }
+
+        public string CrewName { get; set; }
+
+        public List<Student> Students { get; set; }
+    }
+}
---
+++ H2ODatabase/Models/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace H2ODatabase.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+
+    }
+}
